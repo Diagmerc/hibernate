29ba0a422472619cea44be5571cf8dadf8aadd94
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2023-01-31 21:43:29 +0300

    full version

---
+++ src/main/java/dao/AddressDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+import domain.Address;
+import org.hibernate.SessionFactory;
+
+public class AddressDAO extends GenericDAO<Address> {
+    public AddressDAO(SessionFactory sessionFactory) {
+        super(Address.class, sessionFactory);
+    }
+}
